ece2bd25d7f582880b557f3b11a6c4fa90830783
Mouhamet Ndiaye <ahmetndiaye404@gmail.com>
2022-09-21 21:42:50 +0100

    expanding sorter feature

--- server/src/controllers/user/get-user-controller.js
+++ server/src/controllers/user/get-user-controller.js
@@ -1,9 +1,10 @@
 const { getUserService } = require('../../services/user/get-user-service');
 
 const getUserController = async (req, res) => {
+  const { sortBy } = req.query;
   const { username } = res.locals.verified.payload;
   const { blogsNum } = req.query;
-  const result = await getUserService(username, +blogsNum);
+  const result = await getUserService({ username, sortBy, maxNumOfBlogs: +blogsNum || 3 });
 
   if (result.error) res.status(500);
   res.json(result);
--- server/src/services/user/get-user-service.js
+++ server/src/services/user/get-user-service.js
@@ -1,19 +1,20 @@
 const { userProjectedProps } = require('../../constants/user-projected-props');
 const { getCollection } = require('../../utils/get-collection');
 const { getQueryBlogs } = require('../../utils/get-query-blogs');
+const { getSortedBy } = require('../../utils/get-sorted-by');
 
-const getUserService = async (username, maxNumOfBlogs = 3) => {
+const getUserService = async ({ username, maxNumOfBlogs, sortBy }) => {
   try {
     const usersCollection = getCollection('users');
     const blogsCollection = getCollection('blogs');
     const [user] = await usersCollection.find({ username }).project(userProjectedProps).toArray();
 
-    const queryBlogs = getQueryBlogs(user.blogs, maxNumOfBlogs)
-    const userLatestBlogs = await blogsCollection.find({ $or: queryBlogs }).toArray();
+    const queryBlogs = getQueryBlogs(user.blogs, maxNumOfBlogs);
+    const userLatestBlogs = await blogsCollection.find({ $or: queryBlogs }).sort(getSortedBy(sortBy)).toArray();
 
     return { result: { user, userLatestBlogs } };
   } catch (error) {
-    return { error: "server error while fetching user credits" };
+    return { error: 'server error while fetching user credits' };
   }
 };
 
