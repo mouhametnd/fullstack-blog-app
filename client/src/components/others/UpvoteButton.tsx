efbb9a80d7591510ebc014dabbef260d8794d2c2
Mouhamet Ndiaye <ahmetndiaye404@gmail.com>
2022-10-05 10:57:05 +0100

    Project finished hamdulillah

--- client/src/components/others/UpvoteButton.tsx
+++ client/src/components/others/UpvoteButton.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const UpvoteButton = ({ handleClick, total, hasUserVoted }: IProps) => (
   <button
     onClick={handleClick}
-    className={` hover text-cyanGreen-100 transition-colors bg-cyanGreen-100/10  rounded-xl py-2 px-4 w-max h-max cursor-pointer upvote-button transition-opacity gap-3 flex flex-row ${
+    className={` hover text-cyanGreen-100 transition-colors bg-cyanGreen-100/10  rounded-xl py-2 px-4 w-max h-max cursor-pointer upvote-button transition-opacity gap-3 flex flex-row  ${
       hasUserVoted && 'bg-cyanGreen-100 text-white'
     }`}
   >
